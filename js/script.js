6436e08d9e3ed96dc70ab834515d907bfb3e2154
angelo friello <angelofriello01@gmail.com>
2023-10-06 17:23:03 +0200

    ADD BACKGROUND COLOR

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-const gridDOMElement = document.querySelector('.grid')
-console.log(gridDOMElement)
+const gridDOMElement = document.querySelector('.grid');
+console.log(gridDOMElement);
 // - far comparire in console i numeri da 1 a 100
 //     - dichiarare un ciclo
 //         - digliarare un "indice" di partenza
@@ -8,7 +8,7 @@ for (let i = 0;
      i < 100;
 //         - dichiarare un instruzione di incremento
       i++) {
-    let n = i + 1
+    let n = i + 1;
 // - far si che i numeri multipli di 3 compaio la scritta "Fizz"
 //     - dichiarare una variabile che divida tutti i numeri per 3
 //     - dichiarare una variabile che divida tutti i numeri per 5
@@ -16,27 +16,28 @@ for (let i = 0;
 
 if ((n % 3) === 0 && (n % 5) === 0) {
 
-    n = 'FizzBuzz'
+    n = 'FizzBuzz';
 
  }else if ((n % 3) === 0) {
-    n = 'Fizz'
+    n = 'Fizz';
  }else if ((n % 5) === 0) {
-    n = 'Buzz'
+    n = 'Buzz';
  }
- console.log(n)
+ console.log(n);
 
 
 // - far si che i numeri multipli di 5 compaio la scritta "Buzz""
 //     - aggiungere una condizione che dica che se un numero multiplo di 3 da come resto 0 deve comparire la scritta "Buzz"
 
-    const boxString = '<div class="box">' + n + '</div>'
-    const boxDOMElement = document.createElement('div')
-
-	boxDOMElement.classList.add('box')
-	boxDOMElement.innerHTML = n
-	gridDOMElement.append(boxDOMElement)
+    
+    const boxDOMElement = document.createElement('div');
+	boxDOMElement.classList.add('box');
+	boxDOMElement.innerHTML = n;
+	gridDOMElement.append(boxDOMElement);
 	
-
+if( n === 'FizzBuzz'){
+    boxDOMElement.classList.add('bY')
+}
 
 }
 
